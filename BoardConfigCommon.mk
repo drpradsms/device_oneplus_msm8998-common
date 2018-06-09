7b73908bca12d4b947e42350c30ce4abcf2ef671
APOPHIS9283 <steaven.android@gmail.com>
2018-06-08 23:00:12 -0400

    AEX-ify

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,8 +76,9 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
-TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
+TARGET_KERNEL_CONFIG := singularity_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_PATH := $(ANDROID_BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/clang-r328903/bin
 
 # Sepolicy
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
